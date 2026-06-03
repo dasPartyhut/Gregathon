83eebbbba456a67668504f2c5f2c84276f5dd6b9
Julian Schwarz <264668916+dasPartyhut@users.noreply.github.com>
2026-06-03 19:59:40 +0200

    Update reactorManager.lua

--- OpenComputers/programs/reactorManager.lua
+++ OpenComputers/programs/reactorManager.lua
@@ -6,14 +6,19 @@ local rsTank = component.proxy(component.get("30801979"))
 local rsController = component.proxy(component.get("9314a798"))
 local rsStoredEnergy = component.proxy(component.get("8e5d91a3"))
 
+local DEBUG = 1 --Unter Bearbeitung
 local WAITTIME = 60 --takt-zyklus 1min
 local N = 60/WAITTIME*60*24*7  --Takt Zyklen in einer Woche
 local N1 = 60/WAITTIME*60*24   --Takt Zyklen an einem Tag
+local WAITTIMEDEBUG = 1 --takt-zyklus 1s
+local NDEBUG = 60*24*7  --Takt Zyklen in einer Woche
+local N1DEBUG = 60*24   --Takt Zyklen an einem Tag
+local DEBUGCYCLES = 5 
 
 local onThreshold = 7
 local offThreshold = 15
 
-ncCycles = 1
+nCycles = 1
 
 runningCycles = 0
 runningCyclesDay = 0
@@ -32,8 +37,16 @@ controllerActive = 0
 for i = 0, (N-1) do
   runningCyclesWeek_arr[i] = 0
 end
-
-while 1 do
+function runLoop()
+    if DEBUG == 1 then
+        WAITTIME = WAITTIMEDEBUG
+        N = NDEBUG
+        N1 = N1DEBUG
+        return nCycles <= DEBUGCYCLES
+    end
+    return true
+    end
+while runLoop() do
   term.clear()
   print('Executing Reactor Management Cycle No. ' .. tostring(ncCycles))
   print()
@@ -62,35 +75,36 @@ while 1 do
     controllerActive = 0
     print('Reactor turned OFF')
   end
- --Bewegtes Fenster
+  --Bewegtes Fenster
   --Wirf letztes Element raus und schiebe alles um 1
   for i = 0, (N-2) do
     z = N-2 - i
     runningCyclesWeek_arr[z+1] = runningCyclesWeek_arr[z]
   end
-  runningCyclesWeek_arr[0] = controllerActive  
+  runningCyclesWeek_arr[0] = controllerActive
   runningCyclesDay = 0
-  runnignCyclesWeek = 0
+  runningCyclesWeek = 0
   for i = 0, (N-1) do
     runningCyclesWeek = runningCyclesWeek + runningCyclesWeek_arr[i]
-	if i < N1 then
+    if i < N1 then
       runningCyclesDay = runningCyclesDay + runningCyclesWeek_arr[i]
+    end
     if i == (nCycles-1) then break end
   end
 
   weekDiv = N
   dayDiv = N1
   if nCycles < N1 then
-    dayDiv = nCycles  
+    dayDiv = nCycles
   end
   if nCycles < N then
     weekDiv = nCycles
   end
-  
+
   runningCycles = runningCycles + controllerActive
-  runningAVG = 100 * runningCycles / ncCycles
-  runningAVGDay = runningCyclesDay / dayDiv
-  runningAVGWeek = runnignCyclesWeek / weekDiv
+  runningAVG = 100 * runningCycles / nCycles
+  runningAVGDay = 100 * runningCyclesDay / dayDiv
+  runningAVGWeek = 100 * runningCyclesWeek / weekDiv
 
   print()
   print('Fusion Reactor Running Cycles: ' .. tostring(runningCycles))
@@ -98,7 +112,7 @@ while 1 do
   print('Fusion Reactor Average Running Time this day: ' .. tostring(runningAVGDay) .. '%')
   print('Fusion Reactor Average Running Time last week: ' .. tostring(runningAVGWeek) .. '%')
   rsController.setOutput(sides.up, controllerActive*255)
-  
-  ncCycles = ncCycles + 1
+
+  nCycles = nCycles + 1
   os.sleep(WAITTIME)
 end
